24ba9799d9d56f4564fd896e6b176dd4c132785b
kristina-larsen <kristinal@live.dk>
2018-12-08 16:55:20 +0100

    videobibliotek changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,1 @@
-function openNav(){
-  document.getElementById("mynav").style.width = "50%";
-}
-function closeNav(){ /*javascript til at lukke menuen når der trykkes på x */
-  document.getElementById("mynav").style.width = "0%";
-}
+
