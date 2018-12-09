9f1f38aabc94a9e1f4ff7975527f67e93b84b871
kristina-larsen <kristinal@live.dk>
2018-12-09 19:49:32 +0100

    burgermenu

--- js/javascript.js
+++ js/javascript.js
@@ -31,3 +31,10 @@ $( document ).ready(function() {
     });
 
 });//slider slut
+
+function openNav(){
+  document.getElementById("mynav").style.width = "50%";
+}
+function closeNav(){ /*javascript til at lukke menuen når der trykkes på x */
+  document.getElementById("mynav").style.width = "0%";
+}
